c4a4e8557dbbd7a884aa25e7beb1e327a0cf9758
Khushal Sahni <kstheking0@gmal.com>
2020-01-15 11:50:57 +0530

    Pretty cool trick to store two numbers at one position

--- Array/rearrangeArrayAlternatively.cpp
+++ Array/rearrangeArrayAlternatively.cpp
@@ -39,3 +39,31 @@ int main(){
     printf("\n");
   }
 }
+
+//**************************************************ALTERNATE SOLUTION**********************************
+#include <iostream>
+using namespace std;
+int main(){
+  int t;
+  scanf("%d",&t);
+  while(t--){
+    int n,i,max;
+    scanf("%d",&n);
+    long int arr[n];
+    for(i = 0; i < n; i++) scanf("%ld",&arr[i]);
+    i = 0,max = arr[n-1]+1;
+    while(i < n/2){
+      arr[2*i + 1] += ((arr[i]%max) * max);
+      i++;
+    }
+    while(i < n){
+      arr[2*(n - i -1)] += ((arr[i]%max) * max);
+      i++;
+    }
+    for(i = 0; i < n; i++){
+      arr[i] /= max;
+      printf("%ld ",arr[i]);
+    }
+    printf("\n");
+  }
+}
