2978241c73c368fae411fc902cb2d9f215120782
Khushal Sahni <kstheking0@gmail.com>
2020-02-12 07:09:46 +0530

    Maximum of all subarrays of size k

---
+++ Stack and Queue/maximumOfAllSubarraysOfSizeK.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int arr[10000000];
+
+void max_of_subarrays(int n, int k){
+
+    deque<int> q;       // Defining the deque data structure.
+
+    int i = 0;
+    for(i = 0;i<k;i++){     // Running for the first k numbers
+
+        while((!q.empty()) && (arr[i] >= arr[q.back()]))    // Store the biggest number at queue front always
+            q.pop_back();
+
+        q.push_back(i);
+
+    }
+
+    for(;i<n;i++){          // Sliding window technique
+
+        cout << arr[q.front()] << " ";
+
+        while((!q.empty()) && (q.front() <= i-k)) q.pop_front();    // Removing the front if it is no more in the range.
+
+        while((!q.empty()) && (arr[i] >= arr[q.back()])) q.pop_back();  // Pop all the smaller numbers from back.
+
+        q.push_back(i); // Push the current number index
+
+    }
+
+    cout << arr[q.front()] << endl;
+    q.pop_front();
+}
+
+
+int main() {
+
+	int t;
+	cin >> t;       // Inputting the test cases
+
+	while(t--){     // Exhausting all the test cases
+
+	    int n, k;
+	    cin >> n >> k;
+
+	    for(int i = 0;i<n;i++) cin >> arr[i];
+
+	    max_of_subarrays(n, k);
+
+	}
+
+	return 0;
+}
+
+// TIME LIMIT EXCEEDED
+// #include <iostream>
+// #include <set>
+// using namespace std;
+// int main(){
+//   int t;
+//   scanf("%d",&t);
+//   while(t--){
+//     int n,k,i;
+//     scanf("%d %d",&n,&k);
+//     int arr[n];
+//     multiset <int> x;
+//     for(i = 0; i < n; i++) scanf("%d",&arr[i]);
+//     for(i = 0; i < k; i++) x.insert(arr[i]);
+//     cout << (*x.rbegin()) << " ";
+//     for(i = k; i < n; i++){
+//       x.erase(x.lower_bound(arr[i-k]));
+//       x.insert(arr[i]);
+//       cout << (*x.rbegin()) << " ";
+//     }
+//     cout << "\n";
+//   }
+// }
