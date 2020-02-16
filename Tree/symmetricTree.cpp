656f4a569e9b97786e1fb53d5806e0aa6ab40b75
Khushal Sahni <kstheking0@gmail.com>
2020-02-16 09:54:08 +0530

    Check if tree is symmetric

---
+++ Tree/symmetricTree.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+struct Node
+{
+	int key;
+	struct Node* left, *right;
+};
+
+Node* createNode(){
+  Node* node = new Node();
+  node->key = NULL;
+  node->left = NULL;
+  node->right = NULL;
+}
+
+void func(Node* node, bool &allNull, queue <Node *> &q){
+  if(node->left != NULL){
+    q.push(node->left);
+    allNull = false;
+  }
+  else q.push(createNode());
+  if(node->right != NULL){
+    q.push(node->right);
+    allNull = false;
+  }
+  else q.push(createNode());
+}
+
+bool isSymmetric(struct Node* root)
+{
+	if(root == NULL) return true;
+  queue <Node *> q;
+  stack <int> s;
+  Node * node;
+  int i, level = 2;
+  bool allNull = false;
+
+  func(root, allNull, q);
+
+  while(!allNull){
+    allNull = true;
+    for(i = 0; i < level/2; i++){
+      node = q.front();
+      s.push(node->key);
+      func(node, allNull, q);
+      q.pop();
+    }
+    for(i = 0; i < level/2; i++){
+      node = q.front();
+      if(node->key != s.top()) return false;
+      s.pop();
+      func(node, allNull, q);
+      q.pop();
+    }
+    level *= 2;
+  }
+  return true;
+}
