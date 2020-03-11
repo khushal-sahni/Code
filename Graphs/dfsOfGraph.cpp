3f487e80e1f50db990cee5b8519578a7486c70a9
Khushal Sahni <kstheking0@gmail.com>
2020-03-11 11:25:09 +0530

    DFS of graph

---
+++ Graphs/dfsOfGraph.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+void dfs(int s, vector <int> g[], bool vis[]){
+  vis[s] = true;
+  cout << s << " ";
+  for(auto i = g[s].begin(); i != g[s].end(); i++){
+    if(!vis[*i]) dfs(*i,g,vis);
+  }
+}
