7a3085700562bd40f404efc53b560498aa88c3a8
Khushal Sahni <kstheking0@gmail.com>
2020-04-10 13:05:43 +0530

    update

--- Dynamic Programming/shortestCommonSupersequence.cpp
+++ Dynamic Programming/shortestCommonSupersequence.cpp
@@ -41,3 +41,27 @@ int main(){
     cout << a + b - maxLength << "\n";
   }
 }
+
+// another faster solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    string s1,s2; cin>>s1>>s2;
+	    int n=s1.length();
+	    int m=s2.length();
+	    int dp[n+1][m+1];
+	    for(int i=0;i<=n;i++){
+	        for(int j=0;j<=m;j++){
+	            if(i==0 || j==0) dp[i][j]=0;
+	            else if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+	            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+	        }
+	    }
+	    cout<<n+m-dp[n][m]<<endl;
+	}
+	return 0;
+}
