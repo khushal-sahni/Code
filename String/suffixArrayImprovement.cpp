7c27e51b4590cbf1feb794f0ac8b19e41fdc4218
Khushal Sahni <kstheking0@gmail.com>
2020-08-05 15:31:35 +0530

    First improvement suffix array

--- String/suffixArrayImprovement.cpp
+++ String/suffixArrayImprovement.cpp
@@ -16,6 +16,29 @@ using lli  = long long int;
 #define p(x) cout << #x << " is " << (x) << " on Line:" << __LINE__ << " \n";
 #define pl(x) for(auto &y:x) cout << y << " "; cout << "\n";
 
+void rsort(vector <pair <pair <int,int>,int>> &v){
+	int n = v.size();
+	vector <int> cnt(n);
+	for(auto &x: v) ++cnt[x.ff.ss];
+	vector <pair <pair <int,int>,int>> v1(n);
+	vi pos(n);
+	pos[0] = 0;
+	for(int i = 1; i < n; ++i) pos[i] = pos[i-1] + cnt[i-1];
+	for(auto &x:v){
+		v1[pos[x.ff.ss]] = x;
+		++pos[x.ff.ss];
+	}
+	//doing for the first element now
+	fill(cnt.begin(),cnt.end(),0);
+	for(auto &x: v1) ++cnt[x.ff.ff];
+	pos[0] = 0;
+	for(int i = 1; i < n; ++i) pos[i] = pos[i-1] + cnt[i-1];
+	for(auto &x:v1){
+		v[pos[x.ff.ff]] = x;
+		++pos[x.ff.ff];
+	}
+}
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -47,13 +70,14 @@ int main(){
 	  for(int i = 0; i < n; ++i){
 		  a[i] = {{c[i],c[(i+ (1 << k) )%n]},i};
 	  }
-	  sort(a.begin(),a.end());
+	  rsort(a);
 	  for(int i = 0; i < n; ++i) o[i] = a[i].ss;
 	  c[o[0]] = 0;
 	  for(int i = 1; i < n; ++i){
 		  if(a[i].ff == a[i-1].ff) c[o[i]] = c[o[i-1]];
 		  else c[o[i]] = c[o[i-1]] + 1;
 	  }
+	  if(c[o[n-1]] == n-1) break;//sufficient classes
 	  ++k;
   }
   pl(o);
